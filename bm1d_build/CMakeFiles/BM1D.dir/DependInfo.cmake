7bb1f1a116ffd3eea2158066422fcd4d37a63133
ujvarib <balazs.ujvari@science.unideb.hu>
2017-10-30 10:14:14 +0100

    version1.0

---
+++ bm1d_build/CMakeFiles/BM1D.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hallgato9/hall9git/BrownM9/BM1D.cc" "/home/hallgato9/hall9git/BrownM9/bm1d_build/CMakeFiles/BM1D.dir/BM1D.cc.o"
+  "/home/hallgato9/hall9git/BrownM9/src/BM1DProcess.cc" "/home/hallgato9/hall9git/BrownM9/bm1d_build/CMakeFiles/BM1D.dir/src/BM1DProcess.cc.o"
+  "/home/hallgato9/hall9git/BrownM9/src/Plotter.cc" "/home/hallgato9/hall9git/BrownM9/bm1d_build/CMakeFiles/BM1D.dir/src/Plotter.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/root/include"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ bm1d_build/CMakeFiles/BM1D.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BM1D.dir/BM1D.cc.o"
+  "CMakeFiles/BM1D.dir/src/BM1DProcess.cc.o"
+  "CMakeFiles/BM1D.dir/src/Plotter.cc.o"
+  "BM1D.pdb"
+  "BM1D"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BM1D.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
